e91c3f2480ae3fdd7baac2cfe71c6192f4c35bae
gui-bonnis <guibonnis@gmail.com>
2022-10-23 21:47:47 -0300

    Commit inicial

---
+++ src/main/java/com/softplan/simulador_custo/SimuladorCustoApplication.java
@@ -0,0 +1,13 @@
+package com.softplan.simulador_custo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorCustoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorCustoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softplan/simulador_custo/SimuladorCustoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softplan.simulador_custo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorCustoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
